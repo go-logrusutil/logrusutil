83e6cc4e529bd8de1b673a524c25104abcd48d6a
Robert Pajak <pellared@hotmail.com>
2019-11-11 20:43:12 +0100

    rename ctxID to jobID

--- example/goroutines.go
+++ example/goroutines.go
@@ -11,33 +11,33 @@ import (
 
 func registerGo(mux *http.ServeMux) {
 	mux.HandleFunc("/go", func(w http.ResponseWriter, r *http.Request) {
-		ctx := newContextWithID(r.Context())
+		ctx := contextWithNewJobID(r.Context())
 
 		go func(ctx context.Context) {
-			ctx = newContextWithID(ctx)
+			ctx = contextWithNewJobID(ctx)
 			time.Sleep(time.Second)
 			logctx.From(ctx).Info("easy job done")
 		}(ctx)
 
 		go func(ctx context.Context) {
-			ctx = newContextWithID(ctx)
+			ctx = contextWithNewJobID(ctx)
 			time.Sleep(10 * time.Second)
 			logctx.From(ctx).Info("hard job done")
 		}(ctx)
 	})
 }
 
-var ctxIDCounter int64
+var jobIDCounter int64
 
-func newContextWithID(ctx context.Context) context.Context {
+func contextWithNewJobID(ctx context.Context) context.Context {
 	entry := logctx.From(ctx)
-	prevCtxID, wasPrevCtxID := entry.Data["ctxID"].(int64)
-	entry = entry.WithField("ctxID", atomic.AddInt64(&ctxIDCounter, 1))
+	prevJobID, wasPrevJobID := entry.Data["jobID"].(int64)
+	entry = entry.WithField("jobID", atomic.AddInt64(&jobIDCounter, 1))
 
-	if wasPrevCtxID {
-		entry.WithField("prevCtxID", prevCtxID).Info("new ctxID")
+	if wasPrevJobID {
+		entry.WithField("parentJobID", prevJobID).Info("new jobID")
 	} else {
-		entry.Info("new ctxID")
+		entry.Info("new jobID")
 	}
 
 	return logctx.New(ctx, entry)
